9fa6cdf1d9418e53c4cacfeba25b445161b69833
Sascha Peilicke <sascha@peilicke.de>
2016-11-18 19:02:55 +0100

    Merge product flavors
    Not worth the effort

--- mobile/src/main/java/saschpe/birthdays/activity/MainActivity.java
+++ mobile/src/main/java/saschpe/birthdays/activity/MainActivity.java
@@ -38,6 +38,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
@@ -67,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST_ALL = 1;
 
     private AccountArrayAdapter adapter;
+    private AdView adView;
     private CalendarSyncHandler calendarSyncHandler;
     ProgressBar progressBar;
     RecyclerView recyclerView;
@@ -99,6 +104,40 @@ public class MainActivity extends AppCompatActivity {
         } else {
             ActivityCompat.requestPermissions(this, RUNTIME_PERMISSIONS, PERMISSION_REQUEST_ALL);
         }
+
+        // Google Mobile Ads. Look up the AdView as a resource and load a request.
+        MobileAds.initialize(getApplicationContext(),
+                // Not a real secret. If you use that AdMob banner ID in your
+                // projects I will receive the money instead :-)
+                "ca-app-pub-9045162269320751~5472371821");
+
+        adView = (AdView) findViewById(R.id.ad_view);
+        adView.loadAd(new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                // Those numbers are hardly any secret. If you use them in your own
+                // projects I won't get annoying ads, so go ahead :-)
+                .addTestDevice("CB380BC5777E545490CF0D4A435348D7") // Sascha's OnePlus One
+                .addTestDevice("6017F914680B8E8A9B332558F8E53245") // Sascha's Galaxy S2
+                .addTestDevice("284D104E238AB19474B60214A41288B9") // Sascha's Pixel C
+                .build());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        adView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        adView.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        adView.destroy();
+        super.onDestroy();
     }
 
     @Override
