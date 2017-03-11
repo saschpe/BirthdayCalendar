8b29e509335a0b1acf5b388f7c6c96c2c25c21ad
Sascha Peilicke <sascha@peilicke.de>
2017-03-11 10:56:41 +0100

    Update tools

--- tools/inc.functions.sh
+++ tools/inc.functions.sh
@@ -17,12 +17,8 @@ function safe {
     return $status
 }
 
-function get_version {
-    echo $(grep "version \=" $1 | cut -d"\"" -f2)
-}
-
 function get_version_name {
-    echo $(grep "versionName \"" $1 | cut -d"\"" -f2)
+    echo $(grep "versionName '" $1 | cut -d"'" -f2)
 }
 
 function get_version_code {
@@ -31,21 +27,4 @@ function get_version_code {
 
 function fail_build {
     die "\nBUILD FAILED\n"
-}
-
-# Parameters:
-#   FILE
-#   RELEASE
-function release_notes {
-   start_line=$(grep -n "$2" "$1" | cut -d":" -f1)
-   {
-       IFS=''
-       # Skip some lines
-       for ((i=$start_line;i--;)); do read ; done
-       # Read until first blank line
-       while read line ;do
-           if [ -z "$line" ]; then break; fi
-           echo $line
-       done
-   } < "$1"
 }
