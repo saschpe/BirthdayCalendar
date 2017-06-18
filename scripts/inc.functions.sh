7d8f0ca9d91c5db6f5d9b437ce31b09db18edd69
Sascha Peilicke <sascha@peilicke.de>
2017-06-18 17:01:20 +0200

    Rename tools/ to scripts/
    And remove unused bash functions

--- scripts/inc.functions.sh
+++ scripts/inc.functions.sh
@@ -8,23 +8,10 @@ function die {
     exit 1
 }
 
-function safe {
-    "$@"
-    local status=$?
-    if [ $status -ne 0 ]; then
-        die "\nBUILD FAILED\nWhen invoking \"$@\"\n" >&2
-    fi
-    return $status
-}
-
 function get_version_name {
     echo $(grep "versionName '" $1 | cut -d"'" -f2)
 }
 
 function get_version_code {
     echo $(grep "versionCode " $1 | cut -d" " -f10 ) # VERY FLAKY!
-}
-
-function fail_build {
-    die "\nBUILD FAILED\n"
 }
