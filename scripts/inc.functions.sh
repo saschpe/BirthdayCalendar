63771d43d4d51bbc6e92f75851982de8e57b23f8
Sascha Peilicke <sascha@peilicke.de>
2017-09-14 11:08:30 +0200

    Fix version comparison

--- scripts/inc.functions.sh
+++ scripts/inc.functions.sh
@@ -8,6 +8,10 @@ function die {
     exit 1
 }
 
+function version_gt() {
+    test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1";
+}
+
 function get_version_name {
     echo $(grep "versionName '" $1 | cut -d"'" -f2)
 }
